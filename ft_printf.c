9a882affe3d6ca73ce8d799dabaf06ec6505e474
Xueming WANG <xuwang@e2r9p8.42.fr>
2021-05-30 20:03:11 +0200

    1

--- ft_printf.c
+++ ft_printf.c
@@ -6,56 +6,56 @@
 /*   By: xuwang <xuwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/02 17:06:08 by xuwang            #+#    #+#             */
-/*   Updated: 2021/05/30 17:42:31 by xuwang           ###   ########.fr       */
+/*   Updated: 2021/05/30 19:53:08 by xuwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-t_flag  ft_init_flag()
+t_flag	ft_init_flag(void)
 {
-    t_flag flag;
-    flag.minus = 0;
-    flag.prec = -1;
-    flag.start = 0;
-    flag.zero = 0;
-    flag.width = 0;
-    flag.start = 0;
-    return (flag);
+	t_flag	flag;
+
+	flag.minus = 0;
+	flag.prec = -1;
+	flag.start = 0;
+	flag.zero = 0;
+	flag.width = 0;
+	flag.start = 0;
+	return (flag);
 }
 
-int printf_check(const char *format, va_list *list)
+int	printf_check(const char *format, va_list *list)
 {
-    int i;
-    int count;
-    t_flag flag;
-    
-    i = 0;
-    count = 0;
-    while (format[i])
-    {
-        flag = ft_init_flag();
-        if (format[i] == '%' && format[i + 1])
-        {
-            i = flag_check(format, ++i, list, &flag);
-            if (type_list(format[i]))
-                count += type_check(flag.type, list, flag);
-        }
-        else if (format[i] != '%')
-                count += ft_putchar_len(format[i]);
-        i++;
-    }
-    return (count);
+	int		i;
+	int		count;
+	t_flag	flag;
+
+	i = 0;
+	count = 0;
+	while (format[i])
+	{
+		flag = ft_init_flag();
+		if (format[i] == '%' && format[i + 1])
+		{
+			i = flag_check(format, ++i, list, &flag);
+			if (type_list(format[i]))
+				count += type_check(flag.type, list, flag);
+		}
+		else if (format[i] != '%')
+			count += ft_putchar_len(format[i]);
+		i++;
+	}
+	return (count);
 }
 
-int ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
-    int i;
-    va_list list;
-    
-    va_start(list, format);
-    i = printf_check(format, &list);
-    va_end(list);
-    return (i);
-}
+	int		i;
+	va_list	list;
 
+	va_start(list, format);
+	i = printf_check(format, &list);
+	va_end(list);
+	return (i);
+}
--- ft_printf.h
+++ ft_printf.h
@@ -6,59 +6,52 @@
 /*   By: xuwang <xuwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/24 21:35:51 by xuwang            #+#    #+#             */
-/*   Updated: 2021/04/28 17:05:33 by xuwang           ###   ########.fr       */
+/*   Updated: 2021/05/30 20:00:24 by xuwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+# include<stdarg.h>
+# include<stdio.h>
+# include<stdlib.h>
+# include<unistd.h>
 
-
-typedef struct  s_flag
+typedef struct s_flag
 {
-      int type;
-      int zero;
-      int minus;
-      int prec;
-      int start; 
-      int width;
+	int	type;
+	int	zero;
+	int	minus;
+	int	prec;
+	int	start;
+	int	width;
+}				t_flag;
 
-}              t_flag;
-
-// char        *ft_itoa(int n);
-char *ft_itoa(long n);
-char        *ft_itoa_u(unsigned int n);
-char        *ft_itoa_x(unsigned int n, int base, int option);
-char        *ft_itoa_p(unsigned long long n, int base, int option);
-void        ft_putchar(char c);
+char	*ft_itoa(long n);
+char	*ft_itoa_u(unsigned int n);
+char	*ft_itoa_x(unsigned int n, int base, int option);
+char	*ft_itoa_p(unsigned long long n, int base, int option);
+void	ft_putchar(char c);
 size_t	ft_strlen(const char *str);
-int         ft_width(int nb, int len, int zero);
-int         ft_putchar_len(char c);
-int         ft_putstr_len(char *str);
-int         ft_printf(const char *format, ...);
-int         ft_printf_c_percent(char c, t_flag flag);
-int         ft_printf_d_i(int nb, t_flag flag);
-int         ft_printf_p(void *p, t_flag flag);
-int         ft_printf_s(char *str, t_flag flag);
-int         ft_printf_u(unsigned int nb, t_flag flag);
-int         ft_printf_x(unsigned int nb, t_flag flag, int option);
-int         type_list(int c);
-int         flag_list(int c);
-int         flag_check(const char *format, int i, va_list *list, t_flag *flag);
-int         type_check(int c, va_list *list, t_flag flag);
-int         printf_check(const char *format, va_list *list);
-
-t_flag      ft_flag_minus(t_flag flag);
-int         ft_flag_prec(t_flag *flag, int i, const char *format, va_list *list);
-t_flag      ft_flag_start(t_flag flag, va_list *list);
-t_flag      ft_flag_width(t_flag flag, char c);
-
-
+int		ft_width(int nb, int len, int zero);
+int		ft_putchar_len(char c);
+int		ft_putstr_len(char *str);
+int		ft_printf(const char *format, ...);
+int		ft_printf_c_percent(char c, t_flag flag);
+int		ft_printf_d_i(int nb, t_flag flag);
+int		ft_printf_p(void *p, t_flag flag);
+int		ft_printf_s(char *str, t_flag flag);
+int		ft_printf_u(unsigned int nb, t_flag flag);
+int		ft_printf_x(unsigned int nb, t_flag flag, int option);
+int		type_list(int c);
+int		flag_list(int c);
+int		flag_check(const char *format, int i, va_list *list, t_flag *flag);
+int		type_check(int c, va_list *list, t_flag flag);
+int		printf_check(const char *format, va_list *list);
+t_flag	ft_flag_minus(t_flag flag);
+int		ft_flag_prec(t_flag *flag, int i, const char *format, va_list *list);
+t_flag	ft_flag_start(t_flag flag, va_list *list);
+t_flag	ft_flag_width(t_flag flag, char c);
 
 #endif
